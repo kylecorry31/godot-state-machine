91793a241c342fcfba5682890be7a10a26ce4912
Kyle Corry <kylecorry31@gmail.com>
2023-03-12 07:35:49 -0400

    State machine extends state, removes nested SM

--- addons/state_machine/StateMachine.gd
+++ addons/state_machine/StateMachine.gd
@@ -1,10 +1,12 @@
 @icon("./icons/state_machine.svg")
 class_name StateMachine
-extends Node
+extends StateMachineState
 
 @export var initial_state: String
+@export var autostart: bool = false
 
 var state_machine: StateMachineLogic
+var is_running: bool = false
 
 func _ready():
 	var states: Array[StateMachineState] = []
@@ -14,11 +16,34 @@ func _ready():
 	
 	state_machine = StateMachineLogic.new(states)
 	
+	if autostart:
+		start()
+
+func start():
+	if is_running:
+		return
+	is_running = true
+	enter()
+
+func stop():
+	if not is_running:
+		return
+	is_running = false
+	exit()
+
+func _physics_process(delta):
+	if is_running:
+		process(delta)
+
+func enter():
 	if initial_state != null:
 		state_machine.transition_to(initial_state)
 	else:
-		state_machine.transition_to(states[0].get_state_name())
+		state_machine.transition_to(state_machine.states[0].get_state_name())
 
+func exit():
+	state_machine.stop()
 
-func _physics_process(delta):
+func process(delta: float) -> String:
 	state_machine.process(delta)
+	return get_state_name()
